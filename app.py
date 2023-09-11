125925953eb0910f87b5154d90e8dcb71450b262
slemeshevsky <sergey.lemeshevsky@gmail.com>
2023-09-11 15:49:17 +0300

    Добавлено кэширование данных

--- app.py
+++ app.py
@@ -7,32 +7,31 @@ from plotly.subplots import make_subplots
 
 st.set_page_config(layout='wide')
 
-st.sidebar.header("Загрузите Excel-файлы с данными")
-meas_file = st.sidebar.file_uploader("Выберите файл с замерами", type=['xlsx'])
-data_file = st.sidebar.file_uploader("Выберите файл с расчетами", type=['xlsx'])
 
-if (meas_file is not None) and (data_file is not None):
+@st.cache_data
+def load_data(meas_file, data_file):
     meas_wb = openpyxl.load_workbook(meas_file)
     data_wb = openpyxl.load_workbook(data_file)
     wells = list(set(meas_wb.sheetnames) & set(data_wb.sheetnames))
+    measurements = {}
+    computations = {}
+    for well in wells:
+        measurements[well] = pd.read_excel(meas_file, sheet_name=well, index_col=0)
+        computations[well] = pd.read_excel(data_file, sheet_name=well)
+    #st.write("В замерах и результатах :red[нет совпадающих скважин! Загрузите другие файлы.]")
+    return wells, measurements, computations
 
+st.sidebar.header("Загрузите Excel-файлы с данными")
+meas_file = st.sidebar.file_uploader("Выберите файл с замерами", type=['xlsx'])
+data_file = st.sidebar.file_uploader("Выберите файл с расчетами", type=['xlsx'])
+if (meas_file is not None) and (data_file is not None):
+    wells, measurements, computations = load_data(meas_file, data_file)
     if wells is not None:
-        if 'wells' not in st.session_state:
-            st.session_state['wells'] = wells
-    
         st.sidebar.markdown("----")
         st.sidebar.markdown("<h3>Выберите скважину</h3>", unsafe_allow_html=True)
         selected_well = st.sidebar.selectbox('', wells, key='dyn-well-selector')
-
-        well_measurments = pd.read_excel(meas_file, sheet_name=selected_well, index_col=0)
-        if 'meas_file' not in st.session_state:
-            st.session_state['data_file'] = meas_file
-
-        well_computed = pd.read_excel(data_file, sheet_name=selected_well)
-        if 'data_file' not in st.session_state:
-            st.session_state[ 'data_file'] = data_file
-    
-
+        well_measurments = measurements[selected_well]
+        well_computed = computations[selected_well]
         min_depth = well_measurments['Abs'].min()
         cols = well_measurments.columns[1:]
         indices = np.where(well_computed['Abs'] >= min_depth)
@@ -50,24 +49,24 @@ if (meas_file is not None) and (data_file is not None):
             )
 
             computation = go.Contour(
-                    z = well_computed[cols[1:]].values[indices],
-                    y = well_computed['Abs'].loc[indices],
-                    x = cols.values,
-                    colorscale='Jet',
-                    contours=contours)
+                z = well_computed[cols[1:]].values[indices],
+                y = well_computed['Abs'].loc[indices],
+                x = cols.values,
+                colorscale='Jet',
+                contours=contours)
 
             measurements = go.Contour(
-                    z = well_measurments[cols[1:]].values,
-                    y = well_measurments['Abs'],
-                    x = cols.values,
-                    colorscale='Jet',
-                    contours=contours)
+                z = well_measurments[cols[1:]].values,
+                y = well_measurments['Abs'],
+                x = cols.values,
+                colorscale='Jet',
+                contours=contours)
 
             fig1 = make_subplots(
                 rows=2, cols=1,
                 shared_xaxes=True,
                 #    shared_yaxes=True,
-                # vertical_spacing=0.03,
+                    # vertical_spacing=0.03,
                 subplot_titles=('Рассчитанные значения', 'Замеры')
             )
 
@@ -109,5 +108,3 @@ if (meas_file is not None) and (data_file is not None):
             fig2.update_yaxes(title_text='Абсолютная отметка, м')
             fig2.update_layout(width=1000, height=800)
             st.plotly_chart(fig2, theme=None)
-    else:
-        st.write("В замерах и результатах :red[нет совпадающих скважин! Загрузите другие файлы.]")
